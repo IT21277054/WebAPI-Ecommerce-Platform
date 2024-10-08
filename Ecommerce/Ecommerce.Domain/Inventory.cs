24535b09e776046c62c1fd697158d1a9f814c155
Shamry Shiraz <IT21277054@my.sliit.lk>
2024-10-08 12:34:56 +0530

    updated inventory entity

--- Ecommerce/Ecommerce.Domain/Inventory.cs
+++ Ecommerce/Ecommerce.Domain/Inventory.cs
@@ -11,11 +11,12 @@ namespace Ecommerce.Domain;
 
 public class Inventory : BaseEntity<Guid>
 {
+    public Guid VendorId { get; set; }
+    public string Category { get; set; } = string.Empty;
     public Guid ProductId { get; set; }
     public string ProductName { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public double Amount { get; set; }
-    public string Level { get; set; } = string.Empty;
-    public string Catogery { get; set; } = string.Empty;
-    public bool IsAvailble { get; set; }
+    public decimal? UnitPrice { get; set; }
+    public int? Quantity { get; set; }
+    public bool IsAvailable { get; set; } = true;
 }
